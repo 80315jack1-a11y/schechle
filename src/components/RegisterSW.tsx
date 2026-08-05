070d3640e154aa2631083dc73990566f964d2be9
jack_lai 賴冠霖 <jack_lai@accton.com>
2026-08-05 20:05:13 +0800

    feat: 設定靜態匯出 + GitHub Pages 部署支援

--- src/components/RegisterSW.tsx
+++ src/components/RegisterSW.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react'
 export default function RegisterSW() {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').catch(() => {
+      navigator.serviceWorker.register('/schechle/sw.js').catch(() => {
         // SW registration failed, not critical
       })
     }
